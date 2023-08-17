73560e7b19b6d22a7cf2ea1f0e7bf8a045de3528
Into The Unknown <129763215+visionaryvista@users.noreply.github.com>
2023-08-17 09:42:00 +0000

    movement buttons of game

--- bootstrap-project-pages/games.js
+++ bootstrap-project-pages/games.js
@@ -1,2 +1,25 @@
-let changes = document.querySelector("#changes")
-changes.textContent = "hello"
+let dots = document.querySelector(".dots")
+let leftButton = document.querySelector(".left-btn")
+let rightButton = document.querySelector(".right-btn")
+let upButton = document.querySelector(".up-btn")
+let downButton = document.querySelector(".down-btn")
+let leftIncrement = 0
+let upIncrement = 0
+    rightButton.addEventListener("click",function(){
+        leftIncrement +=1
+        dots.style.paddingLeft= leftIncrement+"rem"
+        
+    })
+    leftButton.addEventListener("click",function(){
+        leftIncrement -=1
+        dots.style.paddingLeft= leftIncrement+"rem"
+    })
+    upButton.addEventListener("click",function(){
+        upIncrement -=1
+        dots.style.paddingTop= upIncrement+"rem"
+        
+    })
+    downButton.addEventListener("click",function(){
+        upIncrement +=1
+        dots.style.paddingTop= upIncrement+"rem"
+    })
